4a523dc515803bb2d1221f1faa31e76f41ad3ffd
Kaitokien <luutrungkien107@gmail.com>
2024-07-25 21:15:07 +0700

    Thêm router cho danh mục sản phẩm

---
+++ routers/admin/product-category_router.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const controller = require("../../controllers/admin/product-category_controller");
+
+const multer = require('multer');
+const upload = multer();
+const validate = require('../../validates/admin/product-category_validate');
+const uploadCloudinary = require('../../middlewares/admin/uploadCloudinary_middleware');
+
+router.get('/', controller.index);
+
+router.get('/create', controller.create);
+router.post(
+  '/create', 
+  upload.single('thumbnail'),
+  uploadCloudinary.upload,
+  validate.createPost,
+  controller.createPost
+);
+
+module.exports = router;
