8e7a4d75735e02defb08e526aeef888f824cff9f
Kaitokien <luutrungkien107@gmail.com>
2024-07-25 21:12:09 +0700

    Thêm router cho phần danh mục sản phẩm

--- routers/admin/index_router.js
+++ routers/admin/index_router.js
@@ -2,10 +2,12 @@ const dashboardRouters = require('./dashboard_router.js')
 const systemConfig = require("../../config/system.js")
 const productRouters = require('./product_router.js');
 const deletedProducts = require('./deleted_product_router.js');
+const productCategoryRouter = require('./product-category_router.js');
 
 module.exports = (app) => {
     const PATH_ADMIN = systemConfig.prefixAdmin;
     app.use(PATH_ADMIN + '/dashboard', dashboardRouters);
     app.use(PATH_ADMIN + '/products', productRouters);
     app.use(PATH_ADMIN + '/deleted-products', deletedProducts);
+    app.use(PATH_ADMIN + '/products-category', productCategoryRouter);
 }
