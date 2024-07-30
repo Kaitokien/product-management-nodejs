39af008831634bf19239b3e19bde0c4faf3ed2ec
Kaitokien <luutrungkien107@gmail.com>
2024-07-30 10:19:07 +0700

    Thêm router phân quyền ở dòng 14

--- routers/admin/index_router.js
+++ routers/admin/index_router.js
@@ -3,6 +3,7 @@ const systemConfig = require("../../config/system.js")
 const productRouters = require('./product_router.js');
 const deletedProducts = require('./deleted_product_router.js');
 const productCategoryRouter = require('./product-category_router.js');
+const rolesRouter = require('./roles_router');
 
 module.exports = (app) => {
     const PATH_ADMIN = systemConfig.prefixAdmin;
@@ -10,4 +11,5 @@ module.exports = (app) => {
     app.use(PATH_ADMIN + '/products', productRouters);
     app.use(PATH_ADMIN + '/deleted-products', deletedProducts);
     app.use(PATH_ADMIN + '/products-category', productCategoryRouter);
+    app.use(PATH_ADMIN + '/roles', rolesRouter);
 }
