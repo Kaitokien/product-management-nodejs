ed1b3ad49e7a9c053f23d2fdd267a642fa82d558
Kaitokien <luutrungkien107@gmail.com>
2024-08-02 23:38:14 +0700

    Thêm router cho phần danh sách tài khoản

---
+++ routers/admin/accounts_router.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+const controller = require("../../controllers/admin/accounts_controller");
+const multer = require('multer');
+const uploadCloudinary = require('../../middlewares/admin/uploadCloudinary_middleware');
+const upload = multer();
+const validate = require('../../validates/admin/account_validate');
+
+router.get('/', controller.index);
+
+router.get('/create', controller.create);
+
+router.post(
+  '/create', 
+  upload.single("avatar"),
+  uploadCloudinary.upload, 
+  validate.createPost, 
+  controller.createPost
+);
+
+router.get('/edit/:id', controller.edit);
+
+router.patch(
+  '/edit/:id', 
+  upload.single("avatar"),
+  uploadCloudinary.upload,
+  validate.editPatch,
+  controller.editPatch);
+
+module.exports = router;
