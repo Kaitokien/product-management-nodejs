4375ba4dfde9818812f41a32604cc8b04b2f3000
Kaitokien <luutrungkien107@gmail.com>
2024-08-07 21:39:57 +0700

    Nhúng middleware kiểm tra token

--- routers/admin/index_router.js
+++ routers/admin/index_router.js
@@ -6,14 +6,18 @@ const productCategoryRouter = require('./product-category_router.js');
 const rolesRouter = require('./roles_router');
 const accountsRouter = require('./accounts_router');
 const authRouter = require('./auth_router.js');
+const authMiddleware = require('../../middlewares/admin/auth_middleware.js');
 
 module.exports = (app) => {
     const PATH_ADMIN = systemConfig.prefixAdmin;
-    app.use(PATH_ADMIN + '/dashboard', dashboardRouters);
-    app.use(PATH_ADMIN + '/products', productRouters);
-    app.use(PATH_ADMIN + '/deleted-products', deletedProducts);
-    app.use(PATH_ADMIN + '/products-category', productCategoryRouter);
-    app.use(PATH_ADMIN + '/roles', rolesRouter);
-    app.use(PATH_ADMIN + '/accounts', accountsRouter);
+    app.use(PATH_ADMIN + '/dashboard', 
+      authMiddleware.requireAuth, 
+      dashboardRouters
+    );
+    app.use(PATH_ADMIN + '/products', authMiddleware.requireAuth, productRouters);
+    app.use(PATH_ADMIN + '/deleted-products', authMiddleware.requireAuth, deletedProducts);
+    app.use(PATH_ADMIN + '/products-category', authMiddleware.requireAuth, productCategoryRouter);
+    app.use(PATH_ADMIN + '/roles', authMiddleware.requireAuth, rolesRouter);
+    app.use(PATH_ADMIN + '/accounts', authMiddleware.requireAuth, accountsRouter);
     app.use(PATH_ADMIN + '/auth', authRouter);
 }
