1a3e084bc807eb6d403a659ed08d5d6d4bbe58c1
Kaitokien <luutrungkien107@gmail.com>
2024-08-11 23:42:12 +0700

    Chỉnh sửa lại controller trang chủ

--- controllers/client/home_controller.js
+++ controllers/client/home_controller.js
@@ -1,8 +1,7 @@
 // [GET] /
-
-module.exports.index = (req, res) => {
+module.exports.index = async (req, res) => {
   res.render("client/pages/home/index", {
-    titlePage: "Trang chủ"
+    titlePage: "Trang chủ",
   }); 
 }
 
