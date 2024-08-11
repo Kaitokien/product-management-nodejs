8d6c7b4ebcad332233ed9d547bdd46c74afa5940
Kaitokien <luutrungkien107@gmail.com>
2024-08-11 23:46:47 +0700

    Thêm category vào app cho tất cả các trang bên client

--- routers/client/index_router.js
+++ routers/client/index_router.js
@@ -1,7 +1,9 @@
 const homeRouters = require('./home_router.js')
 const productRouters = require('./product_router.js')
+const categoryMiddleware = require('../../middlewares/client/category_middleware.js');
 
 module.exports = (app) => {
+  app.use(categoryMiddleware.category);
   app.use('/', homeRouters);
 
   //(req, res) => {
@@ -9,4 +11,5 @@ module.exports = (app) => {
   //}
   // Phần comment trên được thay bằng homeRouters
   app.use('/products', productRouters);
+  // Dòng 6: luôn sử dụng categoryMiddleware.category để sau này có nhân bản thêm nữa thì các trang luôn sử dụng categoryMiddleware.category
 }
