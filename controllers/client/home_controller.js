c1fcfb8b691ae1296bdc98fa160643a36b11d4c8
Kaitokien <luutrungkien107@gmail.com>
2024-08-12 23:20:08 +0700

    Thêm sản phẩm nổi bật bên client

--- controllers/client/home_controller.js
+++ controllers/client/home_controller.js
@@ -1,7 +1,18 @@
+const Product = require('../../models/product_model');
+const productsHelper = require('../../helpers/products');
+
 // [GET] /
 module.exports.index = async (req, res) => {
+  // Lấy ra sản phẩm nổi bật
+  const productsFeatured = await Product.find({
+    featured: "1",
+    deleted: false,
+    status: "active"
+  }).limit(2);
+  const newProducts = productsHelper.priceNewProduct(productsFeatured);
   res.render("client/pages/home/index", {
     titlePage: "Trang chủ",
+    productsFeatured: newProducts
   }); 
 }
 
