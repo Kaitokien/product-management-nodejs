da3d570c17356b68fa8392eaaa8e800f0dfeab43
Kaitokien <luutrungkien107@gmail.com>
2024-08-13 16:33:24 +0700

    Xử lý phần danh mục và lấy ra danh mục con

--- controllers/client/product_controller.js
+++ controllers/client/product_controller.js
@@ -1,5 +1,7 @@
 const Product = require("../../models/product_model")
 const productsHelper = require('../../helpers/products');
+const ProductCategory = require('../../models/product-category_model');
+const productCategoryHelper = require('../../helpers/products-category');
 
 // [GET] /products
 module.exports.index = async (req, res) => { //Xóa product đi vì ở bên index_router đã có products rồi
@@ -17,6 +19,7 @@ module.exports.index = async (req, res) => { //Xóa product đi vì ở bên ind
   });
 }
 
+// [GET] /products/:slug
 module.exports.detail = async (req, res) => {
   try {
     const find = {
@@ -35,4 +38,29 @@ module.exports.detail = async (req, res) => {
     req.flash("error", "No product found");
     res.redirect(`/products`);
   }
+}
+
+// [GET] /products/:slugCategory
+module.exports.category = async (req, res) => {
+  const category = await ProductCategory.findOne({
+    slug: req.params.slugCategory,
+    status: "active",
+    deleted: false
+  });
+
+  // Hàm lấy ra các danh mục con
+  const listSubCategory = await productCategoryHelper.getSubCategory(category.id); // Phải dùng await
+  const listSubCategoryId = listSubCategory.map(item => item.id);
+  
+  const products = await Product.find({
+    product_category_id: { $in: [category.id, ...listSubCategoryId] },
+    deleted: false
+  }).sort({ position: "desc" })
+  
+  const newProducts = productsHelper.priceNewProduct(products);
+
+  res.render("client/pages/products/index", {
+    titlePage: category.title,
+    products: newProducts
+  });
 }
