bf86fe72755a542c16fab0bdd666d51ec6683d14
Kaitokien <luutrungkien107@gmail.com>
2024-08-21 15:02:21 +0700

    Làm trang 404

--- index.js
+++ index.js
@@ -49,6 +49,11 @@ app.locals.moment = moment;
 
 routerAdmin(app);
 router(app);
+app.get("*", (req, res) => {
+  res.render("client/pages/errors/404", {
+    pageTitle: "404 Not Found"
+  })
+})
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
