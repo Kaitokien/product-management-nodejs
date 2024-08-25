c2bda3719fa9a30d64a6f81b133f1ca3152ed496
Kaitokien <luutrungkien107@gmail.com>
2024-08-26 01:03:10 +0700

    Chèn icon

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,3 +1,4 @@
+import * as Popper from 'https://cdn.jsdelivr.net/npm/@popperjs/core@^2/dist/esm/index.js'
 // CLIENT_SEND_MESSAGE
 const formSendData = document.querySelector('.chat .inner-form')
 if (formSendData) {
@@ -33,10 +34,35 @@ socket.on("SERVER_RETURN_MESSAGE", (data) => {
   body.scrollTop = body.scrollHeight;
 });
 // End SERVER_RETURN_MESSAGE
+
+// Scroll chat to bottom
 const bodyChat = document.querySelector('.chat .inner-body');
 if(bodyChat) {
   bodyChat.scrollTop = bodyChat.scrollHeight;
 }
-// Scroll chat to bottom
-
 // End Scroll chat to bottom
+
+// Show Icon Chat
+// Show Popup
+const buttonIcon = document.querySelector('.button-icon')
+if (buttonIcon) {
+  const tooltip = document.querySelector('.tooltip');
+  Popper.createPopper(buttonIcon, tooltip);
+
+  buttonIcon.onclick = () => {
+    tooltip.classList.toggle("shown");
+  }
+}
+// End Show Popup
+
+// Insert Icon to Input
+const emojiPicker = document.querySelector('emoji-picker');
+if(emojiPicker) {
+  const inputChat = document.querySelector('.chat .inner-form input[name="content"]');
+  emojiPicker.addEventListener("click", (e) => {
+    const icon = e.detail.unicode;
+    inputChat.value = inputChat.value + icon;
+  });
+}
+// End insert Icon to Input
+// End Show Icon Chat
